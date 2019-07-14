622c79f9ebbd3f9c9b356fc76d54dacff4fe1639
Doroshchuk <doroshchukdaria@gmail.com>
2019-07-14 12:19:45 +0300

    created DataEntity folder -> Company; created GetCompaniesTest() test in TestsCompanyAPI

---
+++ Derivco_API_tests_Doroshchuk/DataEntity/Company.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Derivco_API_tests_Doroshchuk.DataEntity
+{
+    public class Company
+    {
+        [JsonProperty("Id")]
+        public string Id { get; set; }
+
+        [JsonProperty("Name")]
+        public string Name { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+            if (GetType() != obj.GetType()) return false;
+
+            Company company = (Company)obj;
+            return (Id == company.Id) && (Name == company.Name);
+        }
+    }
+}
--- Derivco_API_tests_Doroshchuk/Tests/TestsCompanyAPI.cs
+++ Derivco_API_tests_Doroshchuk/Tests/TestsCompanyAPI.cs
@@ -1,5 +1,9 @@
-﻿using NUnit.Framework;
+﻿using Derivco_API_tests_Doroshchuk.DataEntity;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
 using RestSharp;
+using RestSharp.Serialization.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -29,8 +33,37 @@ namespace Derivco_API_tests_Doroshchuk.Tests
             IRestResponse response = client.Execute(request);
 
             // assert
-            var res = response.StatusCode;
             Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+        }
+
+        [Test]
+        public void GetCompaniesTest()
+        {
+            var expectedCompanyList = new List<Company>()
+            {
+                new Company()
+                {
+                    Id = "1",
+                    Name = "TestCompany"
+                },
+                new Company()
+                {
+                    Id = "2",
+                    Name = "TestCompany1"
+                }
+            };
+            // arrange
+            RestClient client = new RestClient("https://mobilewebserver9-pokertest8ext.installprogram.eu/TestApi/api/automation");
+            RestRequest request = new RestRequest("/companies", Method.GET);
+
+            // act
+            request.AddHeader("authorization", "Bearer " + _token);
+            IRestResponse response = client.Execute(request);
+
+            // assert
+            JArray jsonResponse = (JArray)JsonConvert.DeserializeObject(response.Content);
+            var actualCompanyList = JsonConvert.DeserializeObject<List<Company>>(jsonResponse.ToString());
+            Assert.True(actualCompanyList.SequenceEqual(expectedCompanyList));
         }
     }
 }
