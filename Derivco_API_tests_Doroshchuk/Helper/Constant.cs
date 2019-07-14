6877a1261f7e6be76a05e9fef96e1f1614e4d400
Doroshchuk <doroshchukdaria@gmail.com>
2019-07-14 17:07:13 +0300

    created Helper folder; created Constant class

---
+++ Derivco_API_tests_Doroshchuk/Helper/Constant.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Derivco_API_tests_Doroshchuk.Helpers
+{
+    public static class Constant
+    {
+        public static string BaseURL = "https://mobilewebserver9-pokertest8ext.installprogram.eu/TestApi";
+    }
+}
